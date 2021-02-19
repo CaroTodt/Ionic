274515a7080b3795018de9dc7af8550f4d6b08b2
Carolina <carolina.todt@rosariosoftware.com>
2021-02-19 13:10:44 -0300

    Offers page

--- Ionic/Ionic-Project/ionic-project/src/app/places/places.service.ts
+++ Ionic/Ionic-Project/ionic-project/src/app/places/places.service.ts
@@ -1,40 +1,42 @@
 import { Injectable } from '@angular/core';
-import { Place } from './place.model';
 
+import { Place } from './place.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places : Place[]=[
-    new Place('p1',
-    'Manhattan Mansion',
-    'In the heart of New York cirty',
-    'https://leadingestates.com/wp-content/uploads/2015/11/184-10-manhattan-01.jpg',
-    149.99
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      149.99
     ),
-    new Place('p2',
-    'Iguazu Hotel',
-    'A adventure place',
-    'https://media-cdn.tripadvisor.com/media/photo-s/03/e0/58/b7/costa-iguazu-apart-hotel.jpg',
-    149.99
+    new Place(
+      'p2',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+      189.99
     ),
-    new Place('p3',
-    'Montecarlo Palace',
-    'A great place for relax',
-    'https://media-cdn.tripadvisor.com/media/photo-s/03/e0/58/b7/costa-iguazu-apart-hotel.jpg',
-    150
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+      99.99
     )
+  ];
 
-  ]
-
-
-
-  get places(){
+  get places() {
     return [...this._places];
   }
 
+  constructor() {}
 
-
-  constructor() { }
+  getPlace(id: string) {
+    return {...this._places.find(p => p.id === id)};
+  }
 }
