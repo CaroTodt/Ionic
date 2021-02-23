8cd95b7d530b38c174c406b71e86728b5710c273
Carolina <carolina.todt@rosariosoftware.com>
2021-02-23 12:52:37 -0300

    Menu of the application

--- Ionic/Ionic-Project/ionic-project/src/app/places/offers/edit-offer/edit-offer.module.ts
+++ Ionic/Ionic-Project/ionic-project/src/app/places/offers/edit-offer/edit-offer.module.ts
@@ -6,7 +6,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { EditOfferPageRoutingModule } from './edit-offer-routing.module';
 
-import { EditOfferPage } from './edit-offer.page';
+import { EditOfferPage } from '././edit-offer.page';
 
 @NgModule({
   imports: [
